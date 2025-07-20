c30156bb8228d4d95606dc55c041204a0620c603
Phillip Theriault <phillip.theriault@spellweaver.com>
2025-07-20 08:52:26 -0600

    Added clearScreen() and ask for name

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"fmt"
+    "os"
+    "os/exec"
+    "runtime"
+    "fmt"
 )
 
 func main() {
-	fmt.Println("Hey, I made a Go app!")
+
+    opsys := runtime.GOOS
+
+    var name string
+
+    clearScreen(opsys)
+    fmt.Println("What is your name, brave adventurer?")
+    fmt.Scanln(&name)
+
+    clearScreen(opsys)
+    fmt.Printf("Hello %v. Welcome to Eternia.\n", name)
+}
+
+func clearScreen(opsys string) {
+    var cmd *exec.Cmd
+    switch opsys {
+    case "windows":
+        cmd = exec.Command("cmd", "/c", "cls")
+    default:
+        cmd = exec.Command("clear")
+    }
+    cmd.Stdout = os.Stdout
+    cmd.Run()
 }
