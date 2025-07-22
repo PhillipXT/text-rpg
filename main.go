85f56a796280b64af1d7f8b24994c889b95ac178
Phillip Theriault <phillip.theriault@spellweaver.com>
2025-07-22 13:29:21 -0600

    Changed input method and added command processor

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
     "runtime"
     "fmt"
     "time"
+    "bufio"
+    "strings"
+    "log"
 )
 
 type portalData struct {
@@ -34,20 +37,23 @@ func main() {
     clearScreen()
     fmt.Printf("Hello %v. Welcome to Eternia.\n\n", name)
 
-    currentRoom := 1
+    currentRoom := 0
 
     //portals := getPortalData()
     rooms := getRoomData()
     
     for {
         viewRoom(rooms[currentRoom])
+        fmt.Printf("What do we do now?  ")
         cmd := getCommand()
-        fmt.Printf("%v\n", getResponse(name))
+        clearScreen()
+        processCommand(cmd)
+        fmt.Println(getResponse(name))
         fmt.Printf("We will: %v\n\n", cmd)
-        if currentRoom == 1 {
-            currentRoom = 2
-        } else {
+        if currentRoom == 0 {
             currentRoom = 1
+        } else {
+            currentRoom = 0
         }
     }
 }
@@ -78,22 +84,31 @@ func getResponse(name string) string {
 }
 
 func getCommand() string {
-    cmd := ""
-    fmt.Printf("What do we do now?  ")
-    fmt.Scanln(&cmd)
-    clearScreen()
-    return cmd
+    reader := bufio.NewReader(os.Stdin)
+    cmd, err := reader.ReadString('\n')
+    if err != nil {
+        log.Fatal(err)
+    }
+    return strings.TrimSuffix(cmd, "\n")
 }
+
+func processCommand(cmd string) {
+    switch cmd {
+    case "q", "quit":
+        os.Exit(0)
+    }
+}
+
 
 func getRoomData() (map[int]roomData) {
     r := make(map[int]roomData)
-    r[1] = roomData {
+    r[0] = roomData {
         "You are in a simple room.  There is a desk and chair up against the wall, and a door to the north.",
-        []portalData { portalData { 1, "door", "N", 2 },},
+        []portalData { portalData { 1, "door", "N", 1 },},
     }
-    r[2] = roomData {
+    r[1] = roomData {
         "You are in another room.  There is a bookshelf that is mostly empty. There is a door to the south.",
-        []portalData { portalData { 1, "door", "S", 1 },},
+        []portalData { portalData { 1, "door", "S", 0 },},
     }
     return r
 }
@@ -104,9 +119,10 @@ func getPortalData() map[int]bool {
 
 func viewRoom(room roomData) {
     fmt.Println(room.desc)
-    fmt.Printf("\nVisible exits: ")
+    fmt.Println()
+    fmt.Printf("Visible exits: ")
     for _, portal := range room.portals {
-        fmt.Printf(portal.direction)
+        fmt.Printf(portal.direction + " ")
     }
-    fmt.Println("\n")
+    fmt.Printf("\n\n")
 }
